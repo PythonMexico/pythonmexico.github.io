a0f5779c9195266c90bf919ea09acc7130a89408
Alexandro Colorado <jza@oooes.org>
2016-06-29 16:05:16 -0500

    add img-circle to the profile images

--- static/js/content.py
+++ static/js/content.py
@@ -2,14 +2,37 @@
 # 
 # Load the content to the main_section
 #
-from browser import document, alert
+import time
+from browser import document
+from browser import ajax
 
-def show(ev):
-    alert('click on %s' %ev.currentTarget.id)
+def on_complete(req):
+    if req.status==200 or req.status==0:
+        doc['restult'].html = req.text
+    else:
+        doc['result'].html = "error "+req.text
 
-def show_stop(ev):
-    alert('click on %s' %ev.currentTarget.id)
-    ev.stopPropagation()
+def err_msg():
+    doc['restult'].html = 'server didn\'t reply after %s seconds' %timeout
 
-document['about'].bind('click', show)
+timeout = 4
+
+def fake_qs():
+    return '?foo=$s' %time.time()
+
+def go(url):
+    req = ajax.ajax()
+    req.bind('complete', on_complete)
+    req.set_timeout(timeout, err_msg)
+    req.open('GET',url+fake_qs(),True)
+    req.send
+
+def post(url):
+    req = ajax.ajax()
+    req.set_timeout(timeout, err_msg)
+    req.open('POST', url, True)
+    req.set_header('content-type', 'application/x-www-form-urlencoded')
+    req.send({'foo':34})
+
+document['about'].bind('click', lambda ev:go('ajax_data.html')
 
